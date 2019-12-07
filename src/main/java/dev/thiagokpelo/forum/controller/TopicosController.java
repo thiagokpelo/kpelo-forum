36e34c0e4551341e3f388dbfc20aebb55bf99330
Thiago Capelo <thiagokpelo@gmail.com>
2019-12-07 17:27:25 -0300

    add: pagination

--- src/main/java/dev/thiagokpelo/forum/controller/TopicosController.java
+++ src/main/java/dev/thiagokpelo/forum/controller/TopicosController.java
@@ -8,6 +8,11 @@ import dev.thiagokpelo.forum.model.Topico;
 import dev.thiagokpelo.forum.repository.CursoRepository;
 import dev.thiagokpelo.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -15,7 +20,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -28,12 +32,13 @@ public class TopicosController {
     private CursoRepository cursoRepository;
 
     @GetMapping
-    public List<TopicoDto> lista(String nomeCurso) {
+    public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
+            @PageableDefault(sort = "id", direction = Sort.Direction.ASC, page = 0, size = 10) Pageable paginacao) {
         if (nomeCurso == null) {
-            List<Topico> topicos = topicoRepository.findAll();
+            Page<Topico> topicos = topicoRepository.findAll(paginacao);
             return TopicoDto.converter(topicos);
         } else {
-            List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+            Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
             return TopicoDto.converter(topicos);
         }
     }
--- src/main/java/dev/thiagokpelo/forum/repository/TopicoRepository.java
+++ src/main/java/dev/thiagokpelo/forum/repository/TopicoRepository.java
@@ -1,13 +1,13 @@
 package dev.thiagokpelo.forum.repository;
 
 import dev.thiagokpelo.forum.model.Topico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
-    List<Topico> findByCursoNome(String nomeCurso);
+    Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
 
 //    @Query("SELECT t FROM Topico WHERE t.curso.nome = :nomeCurso")
 //    List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
