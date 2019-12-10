ae98ef912e1a4013a7197c1cce584f6f0d8399a9
Thiago Capelo <thiagokpelo@gmail.com>
2019-12-10 00:20:55 -0300

    add: swagger

--- src/main/java/dev/thiagokpelo/forum/config/seguranca/SegurancaConfig.java
+++ src/main/java/dev/thiagokpelo/forum/config/seguranca/SegurancaConfig.java
@@ -55,5 +55,7 @@ public class SegurancaConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
     }
 }
